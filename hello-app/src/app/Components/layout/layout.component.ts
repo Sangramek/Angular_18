dd5ff20cb3f6069680a74e4fdb6d4e08aab0a27c
Sangramek <Sangram.ekshinge2011@gmail.com>
2024-07-28 15:02:36 +0530

    updated all topics learnings

--- hello-app/src/app/Components/layout/layout.component.ts
+++ hello-app/src/app/Components/layout/layout.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
-
 @Component({
-  selector: 'app-root',
+  selector: 'app-layout',
   standalone: true,
   imports: [RouterOutlet,RouterLink],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
 })
-export class AppComponent {
-  title = 'hello-app';
+export class LayoutComponent {
+
 }
